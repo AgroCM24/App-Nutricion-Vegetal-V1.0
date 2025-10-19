187ce3baf6c1dcdf3e85a99ff2ac2f660defcfa3
AgroCM24 <carlos.maldonado20@uabc.edu.mx>
2025-10-19 01:50:55 -0700

    Update streamlit_nutricion.py

--- streamlit_nutricion.py
+++ streamlit_nutricion.py
@@ -75,9 +75,6 @@ try:
     creds = ServiceAccountCredentials.from_json_keyfile_name("credenciales.json", scope)
     client = gspread.authorize(creds)
     sheet = client.open("EntrenamientoIa").sheet1
-except Exception as e:
-    st.warning(" ")
-    sheet = None
 
 # ============================================
 # SECCIÓN A - Interpretación clásica
